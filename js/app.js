f618f4c462319db48abbfb155e57a3f57fbec9cb
SvetlanaM <svetlana@kompilator.cz>
2022-02-28 23:06:30 +0100

    Basic formatting

--- js/app.js
+++ js/app.js
@@ -1,76 +1,68 @@
-
-// create our angular app and inject ngAnimate and ui-router 
+// create our angular app and inject ngAnimate and ui-router
 // =============================================================================
-angular.module('formApp', ['ngAnimate', 'ui.router', 'elif'])
+angular
+	.module('formApp', ['ngAnimate', 'ui.router', 'elif'])
 
-// configuring our routes 
-// =============================================================================
-.config(function($stateProvider, $urlRouterProvider) {
-    
-    $stateProvider
-    
-        // route to show our basic form (/form)
-        .state('form', {
-            url: '/form',
-            templateUrl: 'form.html',
-            controller: 'formController'
-        })
-        
-        // nested states 
-        // each of these sections will have their own view
-        // url will be nested (/form/profile)
-        .state('form.whoyouare', {
-            url: '/whoyouare',
-            templateUrl: 'form-profile.html'
-        })
-        
-        // url will be /form/interests
-        .state('form.industry', {
-            url: '/industry',
-            templateUrl: 'form-industry.html'
-        })
-        
-        // url will be /form/payment
-        .state('form.service', {
-            url: '/service',
-            templateUrl: 'form-service.html'
-        })
+	// configuring our routes
+	// =============================================================================
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
 
-       
+			// route to show our basic form (/form)
+			.state('form', {
+				url: '/form',
+				templateUrl: 'form.html',
+				controller: 'formController',
+			})
 
-        .state('form.requirements', {
-            url: '/requirements',
-            templateUrl: 'form-requirements.html'
-        })
+			// nested states
+			// each of these sections will have their own view
+			// url will be nested (/form/profile)
+			.state('form.whoyouare', {
+				url: '/whoyouare',
+				templateUrl: 'form-profile.html',
+			})
 
-         .state('form.other', {
-            url: '/other',
-            templateUrl: 'other.html'
-        })
+			// url will be /form/interests
+			.state('form.industry', {
+				url: '/industry',
+				templateUrl: 'form-industry.html',
+			})
 
-        .state('form.summary', {
-            url: '/summary',
-            templateUrl: 'summary.html'
-        });
-       
-       
-       
-    // catch all route
-    // send users to the form page 
-    $urlRouterProvider.otherwise('/form/whoyouare');
-})
+			// url will be /form/payment
+			.state('form.service', {
+				url: '/service',
+				templateUrl: 'form-service.html',
+			})
 
-// our controller for the form
-// =============================================================================
-.controller('formController', function($scope) {
-    
-    // we will store all of our form data in this object
-    $scope.formData = {};
-    
-    // function to process the form
-    $scope.processForm = function() {
-        alert('Thank you for your order!');  
-    };
-    
-});
+			.state('form.requirements', {
+				url: '/requirements',
+				templateUrl: 'form-requirements.html',
+			})
 
+			.state('form.other', {
+				url: '/other',
+				templateUrl: 'other.html',
+			})
+
+			.state('form.summary', {
+				url: '/summary',
+				templateUrl: 'summary.html',
+			});
+
+		// catch all route
+		// send users to the form page
+		$urlRouterProvider.otherwise('/form/whoyouare');
+	})
+
+	// our controller for the form
+	// =============================================================================
+	.controller('formController', function ($scope) {
+		// we will store all of our form data in this object
+		$scope.formData = {};
+
+		// function to process the form
+		$scope.processForm = function () {
+			alert('Thank you for your order!');
+		};
+	});
